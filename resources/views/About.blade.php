dcfd25db0743b66ef32db514a2f16fc51668bc50
koeuk <koeukkos@gmail.com>
2024-11-29 00:18:18 +0700

    upload file laravel tailwind

---
+++ resources/views/About.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    <h2>This is About Page</h2>
+</x-layout>
---
+++ resources/views/Home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
+    <h2>This is Home Page</h2>
+</x-layout>
