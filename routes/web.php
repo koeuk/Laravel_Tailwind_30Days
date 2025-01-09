a17d3519c0f5151283699be8ccba99945663461c
koeuk <koeukkos@gmail.com>
2025-01-09 22:50:29 +0700

    Days 13 feat: add Laravel Debugbar for improved debugging; update Job view for better layout and employer display; optimize job retrieval in routes

--- routes/web.php
+++ routes/web.php
@@ -5,36 +5,24 @@ use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
-    // $jobs = Job::all();
-
-    // dd($jobs[1]->salary ?? 'No job found');
 });
-// ----------------------------------------------------------------
+
 Route::get('/about',    function () {
     return view('about');
 });
-// ----------------------------------------------------------------
 
 Route::get('/contact', function () {
     return view('contact');
 });
-// ----------------------------------------------------------------
-
-
-// ----------------------------------------------------------------
 
 Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->get();
     return view('jobs', [
-        'jobs' => Job::all(),
+        'jobs' => $jobs,
     ]);
 });
-// ----------------------------------------------------------------
 
 Route::get('/jobs/{id}', function ($id) {
-    // dd($id);
     $job = Job::findByid($id);
-    // Use Arr::first to find the job by id
-    // $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-    // Return view with the job
     return view('job', ['job' => $job]);
 });
