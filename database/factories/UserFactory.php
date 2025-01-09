d20862ccf82f627a0a0ecaaa1e32eb03f8cf5c5e
koeuk <koeukkos@gmail.com>
2025-01-10 01:51:46 +0700

    Days 15 feat: add JobSeeder for seeding job data; update DatabaseSeeder to include JobSeeder and comment out admin field in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'admin' => false,
+            // 'admin' => false,
         ];
     }
 
@@ -49,7 +49,7 @@ class UserFactory extends Factory
     public function admin(): static
     {
         return $this->state(fn (array $attributes) => [
-            'admin' => true,
+            // 'admin' => true,
         ]);
     } // User::factory()->admin()->create();
 }
