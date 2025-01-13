64bf3bb348a4d29d8b794a59d2a4ed1b0e0ee028
koeuk <koeukkos@gmail.com>
2025-01-13 22:39:57 +0700

    Days 18 - feat: implement job editing and deletion functionality; add edit job view and update routes for job management

--- resources/views/Jobs/Show.blade.php
+++ resources/views/Jobs/Show.blade.php
@@ -2,13 +2,22 @@
     <x-slot:heading>
         Job Details!
     </x-slot:heading>
-{{-- you can use this condition if you don't want use condition on job.php --}}
-    {{-- @if($job) --}}
-        <ul>
-            <h2 class="font-bold text-lime-700">{{ $job['title'] }}</h2>
-            <p>{{ $job['salary'] }}</p>
-        </ul>
+    {{-- you can use this condition if you don't want use condition on job.php --}}
+    {{-- @if ($job) --}}
+    <ul>
+        <h2 class="font-bold text-lime-700">{{ $job['title'] }}</h2>
+        <p>{{ $job->salary }}</p>
+    </ul>
     {{-- @else
         <p>Job not found.</p>
     @endif --}}
+    <div class="d-flex">
+        <x-button class="mt-10" href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+        <button type="button" form="delete-form"
+            class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-red-500 bg-white border border-gray-300 leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 rounded transition ease-in-out duration-150 dark:bg-gray-200 dark:border-gray-600 dark:active:bg-gray-700 dark:focus:border-blue-800 ">Delete</button>
+    </div>
+    <form method="POST" action="/jobs/{{ $job->id }}" id="delete-form" class="hidden">
+        @csrf
+        @method('DELETE')
+    </form>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,50 +3,86 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
+// Get home
 Route::get('/', function () {
     return view('home');
 });
-
+// Get about
 Route::get('/about',    function () {
     return view('about');
 });
-
+// Get Contact 
 Route::get('/contact', function () {
     return view('contact');
 });
 
+// Index Page
 Route::get('/jobs', function () {
-    // $jobs = Job::with('employer')->paginate(3);
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
+    // $jobs = Job::with('employer')->paginate(3);
     // $jobs = Job::with('employer')->cursorPaginate(3);
     return view('jobs.index', [
         'jobs' => $jobs,
     ]);
 });
 
-Route::get('/jobs/create', function() {
+// Create Job
+Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.show', ['job' => $job]);
-});
 
+// Store Job
 Route::post('/jobs', function () {
     // Validation
     request()->validate([
-        'title' =>['required', 'min:5'],
-        'salary' =>['required'],
+        'title' => ['required', 'min:5'],
+        'salary' => ['required'],
     ]);
 
     // Create new job
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
-        'employer_id' => 2,
+        'employer_id' => 1,
     ]);
-
     // Redirect to job show page
     return redirect('/jobs');
 });
 
+// Show Job
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.show', ['job' => $job]);
+});
+// Edit Job
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update Job
+Route::patch('jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:5'],
+        'salary' => ['required'],
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs');
+});
+
+// Delete Job
+Route::delete('jobs/{id}', function ($id) {
+    $job = Job::findOrFail($id);
+
+    $job->delete();
+
+    return redirect('/jobs');
+});
